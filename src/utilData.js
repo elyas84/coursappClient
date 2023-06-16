48cde6b2512490f47c83feeabb906db946ac4630
elyas84 <57195521+elyas84@users.noreply.github.com>
2023-06-16 22:32:58 +0200

    Update utilData.js

--- src/utilData.js
+++ src/utilData.js
@@ -1,25 +1,91 @@
+mport { BiSquareRounded, BiSearch } from "react-icons/bi";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { FiUserPlus } from "react-icons/fi";
+import { AiOutlineLogout, AiOutlineLogin } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
 
-export const navitems = [
+export const navitemsPublic = [
   {
     path: "/",
+    icon: <BiSquareRounded />,
     title: "home",
   },
   {
     path: "/contact",
+    icon: <RiCustomerService2Fill />,
     title: "contact",
+  },
+
+  {
+    path: "/search",
+    icon: <BiSearch />,
+    title: "search",
   },
   {
     path: "/login",
+    icon: <AiOutlineLogin />,
     title: "login",
+  },
+];
+export const navitemsForAdmin = [
+  {
+    path: "/",
+    icon: <BiSquareRounded />,
+    title: "home",
+  },
+  {
+    path: "/register",
+    icon: <FiUserPlus />,
+    title: "register",
+  },
+
+  {
+    path: "/profile",
+    icon: <FaUserCircle />,
+    title: "profile",
   },
   {
     path: "/search",
+    icon: <BiSearch />,
     title: "search",
+  },
+  {
+    path: "/",
+    icon: <AiOutlineLogout />,
+    title: "logout",
   },
 ];
 
+export const navitemsForUser = [
+  {
+    path: "/",
+    icon: <BiSquareRounded />,
+    title: "home",
+  },
+  {
+    path: "/contact",
+    icon: <RiCustomerService2Fill />,
+    title: "contact",
+  },
+
+  {
+    path: "/search",
+    icon: <BiSearch />,
+    title: "search",
+  },
+  {
+    path: "/profile",
+
+    icon: <FaUserCircle />,
+    title: "profile",
+  },
+  {
+    path: "/",
+    icon: <AiOutlineLogout />,
+    title: "logout",
+  },
+];
 export const welcomePageBg = {
   image:
     "https://images.unsplash.com/photo-1560317620-1ba88ae56e7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
 };
-
